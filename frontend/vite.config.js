7e050c6c7630c106dd24b4892b0d849aac67c05b
tmonka9 <tmonka9@outlook.com>
2026-09-23 00:41:31 -0700

    a

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    host: true,
+    port: 7173,
     proxy: {
-      '/api': 'http://localhost:5000',
+      '/api': 'http://127.0.0.1:5000',
     },
   },
 });
